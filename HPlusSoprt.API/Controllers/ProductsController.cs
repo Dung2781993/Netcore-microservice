d5758279a9cd65b50f420b8066b088764ee42c29
Dung2781993@gmail.com <Dung2781993@gmail.com>
2021-03-17 16:59:56 +1100

    Setting up model

---
+++ HPlusSoprt.API/Controllers/ProductsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HPlusSoprt.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public void GetProducts() { }
+    }
+}
