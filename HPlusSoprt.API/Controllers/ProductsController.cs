b9b1fad52bfcc6545907b89342f2e1fbdeaff1ab
NCLAN\lndu <Ngoc Dung Le>
2021-03-18 12:43:15 +0700

    Update GetAllProducts Controller

--- HPlusSoprt.API/Controllers/ProductsController.cs
+++ HPlusSoprt.API/Controllers/ProductsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HPlusSoprt.API.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,7 +12,17 @@ namespace HPlusSoprt.API.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
+        private readonly ShopContext _context;
+
+        public ProductsController(ShopContext context)
+        {
+            _context = context;
+            context.Database.EnsureCreated();
+        }
+
         [HttpGet]
-        public void GetProducts() { }
+        public IEnumerable<Product> GetAllProducts() {
+            return _context.Products.ToArray();
+        }
     }
 }
